f64b631f649804f5186a09be2dcd9eb60a59bfe8
zjarhdtjdrb <81483103+zjarhdtjdrb@users.noreply.github.com>
2023-11-21 09:16:27 +0900

    [level unrated] Title: 숫자 비교하기, Time: 0.13 ms, Memory: 31.3 MB -BaekjoonHub

--- 프로그래머스/unrated/120807. 숫자 비교하기/숫자 비교하기.cs
+++ 프로그래머스/unrated/120807. 숫자 비교하기/숫자 비교하기.cs
@@ -2,13 +2,6 @@ using System;
 
 public class Solution {
     public int solution(int num1, int num2) {
-        if(num1 == num2)
-        {
-            return 1;
-        }
-        else
-        {
-            return -1;
-        }
+        return num1 == num2 ? 1 : -1;
     }
 }
