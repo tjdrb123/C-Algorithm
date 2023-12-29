2474de4274ad5e356f62b60d3cf82cc4d5ddf74f
tjdrb123 <81483103+tjdrb123@users.noreply.github.com>
2023-12-29 09:44:50 +0900

    [level 1] Title: 수박수박수박수박수박수?, Time: 0.21 ms, Memory: 31.2 MB -BaekjoonHub

--- 프로그래머스/1/12922. 수박수박수박수박수박수？/수박수박수박수박수박수？.cs
+++ 프로그래머스/1/12922. 수박수박수박수박수박수？/수박수박수박수박수박수？.cs
@@ -2,12 +2,7 @@ public class Solution {
     public string solution(int n) {
         string answer = "";
         for (int i = 0; i < n; i++)
-        {
-            if (i % 2 == 0)
-                answer += "수";
-            else
-                answer += "박";
-        }
+            answer += (i % 2 == 0) ? '수' : '박';
         return answer;
     }
 }
