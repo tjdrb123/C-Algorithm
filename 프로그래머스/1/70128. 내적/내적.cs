a46e6f306b566136241382c53739a2863bf4c538
tjdrb123 <81483103+tjdrb123@users.noreply.github.com>
2024-01-02 09:28:45 +0900

    [level 1] Title: 내적, Time: 0.16 ms, Memory: 31.5 MB -BaekjoonHub

--- 프로그래머스/1/70128. 내적/내적.cs
+++ 프로그래머스/1/70128. 내적/내적.cs
@@ -3,8 +3,13 @@ using System;
 public class Solution {
     public int solution(int[] a, int[] b) {
         int answer = 0;
-        for(int i = 0; i< a.Length; i ++)
-            answer += a[i] * b[i];
+        if (a.Length == b.Length)
+        {
+            for (int i = 0; i < a.Length; ++i)
+            {
+                answer += a[i] * b[i];
+            }
+        }
         return answer;
     }
 }
